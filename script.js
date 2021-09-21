80c05ec4e0687333adf76c0d74135efd97ee8588
Maurice Collier <mauricecollier84@yahoo.com>
2021-09-21 10:52:39 -0400

    Bonus 4 & 5.

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ console.log (29);
 // Numbers, Strings, Booleans, Null and Undefined
 
 // 3
-// Adding, Subtracting, Multiplying, Dividing, Exponentiation and Modulus
+// Adding (+), Subtracting(-), Multiplying(*), Dividing(/), Exponentiation(**) and Modulus(%)
 
 // 4
 console.log(5%2);
 console.log(5%4);
 
 // 5
-console.log("'I'M MARY POPPINS, Y'ALL!'");
+console.log('I\'M MARY POPPINS, Y\'ALL!');
 
 // 6
 console.log("Hawk" + "eye");
@@ -32,4 +32,20 @@ console.log(5**4-9);
 // Bonus 3
 console.log(("I".length % "Am".length) - ("Iron".length % "Man".length));
 
+// Bonus 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+// Can also use let or const instead of var
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
 
