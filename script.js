3194107391b77a3fa5a682033e2580bba42e1f08
Maurice Collier <mauricecollier84@yahoo.com>
2021-09-20 23:34:13 -0400

    Created the JSBasicExercise file without the bonus.

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1
+console.log (29);
+
+// 2
+// Numbers, Strings, Booleans, Null and Undefined
+
+// 3
+// Adding, Subtracting, Multiplying, Dividing, Exponentiation and Modulus
+
+// 4
+console.log(5%2);
+console.log(5%4);
+
+// 5
+console.log("'I'M MARY POPPINS, Y'ALL!'");
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Maurice".length + "Collier".length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
